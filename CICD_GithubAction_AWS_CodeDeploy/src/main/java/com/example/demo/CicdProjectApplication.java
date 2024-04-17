efc1f1fcc503c1d568ec5b5b4453ef06627446eb
김민규 <min1925k@gmail.com>
2024-04-17 23:31:01 +0900

    #0 CICD with GithubAction

---
+++ CICD_GithubAction_AWS_CodeDeploy/src/main/java/com/example/demo/CicdProjectApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CicdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdProjectApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloWorldController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Check the CI/CD pipeline in action!!";
+	}
+}
