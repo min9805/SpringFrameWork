7c2a0bb16da16a7f79ddf5688eca8399fd262ba6
김민규 <min1925k@gmail.com>
2024-04-19 19:22:43 +0900

    #3 WebSocket with STOMP

---
+++ WebSocket_Chat/src/main/java/com/min9805/chat/dto/ChatDto.java
@@ -0,0 +1,11 @@
+package com.min9805.chat.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ChatDto {
+
+    private Long chatRoomId;
+    private String sender;
+    private String message;
+}
