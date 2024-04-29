5fec538d912e268fbef938c5c02dc9733947c269
Mingyu <min1925k@gmail.com>
2024-04-30 01:40:39 +0900

    #10 SQL 데이터 삽입 분리

--- Test_DataBase/src/test/java/com/min9805/demo/service/ProductServiceTest.java
+++ Test_DataBase/src/test/java/com/min9805/demo/service/ProductServiceTest.java
@@ -2,23 +2,39 @@ package com.min9805.demo.service;
 
 import com.min9805.demo.domain.Product;
 import com.min9805.demo.domain.ProductRepository;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Sql(scripts = {"/user.sql"})
 class ProductServiceTest {
     @Autowired
     private ProductService productService;
 
-    @Autowired
-    private ProductRepository productRepository;
+    @Test
+    @Sql(scripts = {"/product.sql"})
+    void createProduct() {
+        // Given
+        Long userId = 1L;
+        String name = "product";
+        String description = "description";
+        int price = 1000;
+
+        // When
+        Product product = productService.createProduct(userId, name, description, price);
+
+        // Then
+        assertThat(product.getName()).isEqualTo(name);
+    }
 
     @Test
-    void createProduct() {
+    void createProduct2() {
         // Given
         Long userId = 1L;
         String name = "product";
---
+++ Test_DataBase/src/test/resources/product.sql
@@ -0,0 +1,5 @@
+-- product.sql
+
+INSERT INTO product (user_id, name, description, price) VALUES
+        (1, 'Product 1', 'Product 1 Description', 100),
+        (2, 'Product 2', 'Product 2 Description', 200);
