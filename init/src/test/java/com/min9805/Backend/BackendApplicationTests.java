4d4630336721282e6292fe3334770e4059e7d402
Mingyu <min1925k@gmail.com>
2024-10-16 17:50:08 +0900

    feat: Init Project

---
+++ init/src/test/java/com/min9805/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min9805.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
